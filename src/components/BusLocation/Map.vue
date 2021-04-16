9346427138e1cd22ef41047e530354958db9e2cc
Jose Sanchez <sanchezpepe97@gmail.com>
2021-04-16 00:54:39 -0500

    added tooltip to id the current bus

--- src/components/BusLocation/Map.vue
+++ src/components/BusLocation/Map.vue
@@ -14,15 +14,21 @@
         :attribution="attribution"
       />
 
-      <l-marker :lat-lng="center" :icon="icon"> </l-marker>
+      <l-marker :lat-lng="center" :icon="icon">
+        <l-tooltip :options="tooltipOptions">
+          <strong>
+            Barranca del Muerto
+          </strong>
+        </l-tooltip>
+      </l-marker>
     </l-map>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import { latLng, icon } from "leaflet";
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { latLng, icon, point } from "leaflet";
+import { LMap, LTileLayer, LMarker, LTooltip } from "vue2-leaflet";
 import customIcon from "../../assets/chems.png";
 
 export default {
@@ -30,7 +36,8 @@ export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LMarker,
+    LTooltip
   },
   data() {
     return {
@@ -43,6 +50,7 @@ export default {
       zoom: 16.5,
       center: latLng(19.34508, -99.200004),
       busCoordinates: null,
+      tooltipOptions: { permanent: true, opacity: 1, offset: point(35, -10) },
       url:
         "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiampzYW5jaGV6IiwiYSI6ImNra25jdDhweDE0ZXgyb29jeHRkaW10Z3AifQ.5sV_IKha4TaUpoOBt79H7g",
       attribution:
