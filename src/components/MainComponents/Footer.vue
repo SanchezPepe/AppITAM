01657e9ff3951c002a93b20d65f213ec7d6ca0ba
Jose Sanchez <sanchezpepe97@gmail.com>
2021-04-15 22:03:26 -0500

    redisigned footer

--- src/components/MainComponents/Footer.vue
+++ src/components/MainComponents/Footer.vue
@@ -8,18 +8,10 @@
 
         <v-spacer></v-spacer>
 
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-title>
-
-      <v-card-text class="py-2 white--text text-center">
-        <strong>
-          {{ new Date().getFullYear() }} - Laboratorio de Desarrollo Web ITAM
+        <strong class="subheading">
+          Lab Web ITAM - {{ new Date().getFullYear() }}
         </strong>
-      </v-card-text>
+      </v-card-title>
     </v-card>
   </v-footer>
 </template>
