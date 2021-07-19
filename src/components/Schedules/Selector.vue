51e8e9078d1fb06a481d29ea707fee23ad18081d
Pepe Sanchez <sanchezpepe97@gmail.com>
2021-07-18 22:53:52 -0500

    reorganized calendar view and removed syllabuses

--- src/components/Schedules/Selector.vue
+++ src/components/Schedules/Selector.vue
@@ -1,183 +1,189 @@
 <template>
   <v-container>
-    <v-container>
-      <strong>
-        Se muestran horarios para el semestre OTOÑO 2021
-      </strong>
-      <v-row style="margin-top: 0px">
-        <v-col cols="9">
-          <v-autocomplete
-            :items="items"
-            :error="error"
-            v-model="values"
-            outlined
-            multiple
-            dense
-            chips
-            deletable-chips
-            small-chips
-            label="Materia"
-          ></v-autocomplete>
-        </v-col>
+    <v-card class="mx-auto overflow-hidden">
+      <v-app-bar color="teal darken-3" dark>
+        <app-bar-title>
+          Horarios para el semestre OTOÑO 2021
+        </app-bar-title>
+        <v-spacer></v-spacer>
 
-        <v-col cols="3" align="end">
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                fab
-                dark
-                small
-                v-bind="attrs"
-                v-on="on"
-                color="success"
-                class="mr-4"
-                rounded
-                @click="addCourses"
-              >
-                <v-icon>
-                  mdi-magnify
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Buscar horarios</span>
-          </v-tooltip>
-
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                fab
-                dark
-                small
-                v-bind="attrs"
-                v-on="on"
-                color="primary"
-                class="mr-4"
-                rounded
-                :loading="isSelecting"
-                @click="uploadSchedule"
-              >
-                <v-icon>
-                  mdi-upload
-                </v-icon>
-                <input
-                  ref="uploader"
-                  class="d-none"
-                  type="file"
-                  accept="application/json"
-                  @change="onFileChanged"
-                />
-              </v-btn>
-            </template>
-            <span>Cargar Horario</span>
-          </v-tooltip>
-
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="blue darken-4"
-                class="mr-4"
-                fab
-                dark
-                small
-                v-bind="attrs"
-                v-on="on"
-                @click="exportSchedule"
-              >
-                <v-icon dark>
-                  mdi-download
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Descargar</span>
-          </v-tooltip>
-
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                class="mr-4"
-                fab
-                dark
-                small
-                color="warning"
-                v-bind="attrs"
-                v-on="on"
-                @click="resetCal"
-              >
-                <v-icon dark>
-                  mdi-delete
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Borrar todo</span>
-          </v-tooltip>
-        </v-col>
-      </v-row>
-      <Checkout :selectedCourses="summary" />
-    </v-container>
-
-    <!-- POP UP DIALOG FOR ADDING COURSES -->
-    <v-dialog
-      v-model="dialog"
-      fullscreen
-      hide-overlay
-      transition="dialog-bottom-transition"
-    >
-      <v-card>
-        <v-toolbar dark color="primary">
-          <v-btn icon dark @click="dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-          <v-toolbar-title>Seleccionar horarios</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <v-btn dark text @click="submitCourses()">
-              Agregar cursos
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+              :loading="isSelecting"
+              @click="uploadSchedule"
+            >
+              <v-icon>
+                mdi-upload
+              </v-icon>
             </v-btn>
-            <v-btn dark text @click="dialog = false">
-              Cancelar
-            </v-btn>
-          </v-toolbar-items>
-
-          <template v-slot:extension>
-            <!-- TABS FOR EACH COURSE -->
-
-            <!-- TAB HEADER -->
-            <v-tabs v-model="tab" align-with-title>
-              <v-tabs-slider color="yellow"></v-tabs-slider>
-              <v-tab v-for="value in values" :key="value">
-                {{ value.substring(10, value.length) }}
-              </v-tab>
-            </v-tabs>
           </template>
-        </v-toolbar>
+          <span>Cargar horario</span>
+        </v-tooltip>
 
-        <!-- TAB BODY -->
-        <v-tabs-items v-model="tab">
-          <v-tab-item v-for="(selected, s) in values" :key="s">
-            <v-item-group>
-              <v-container fluid>
-                <v-row no-gutters>
-                  <v-col
-                    v-for="groupNumber in Object.keys(courses[selected])"
-                    :key="groupNumber"
-                    cols="12"
-                    sm="3"
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click="exportSchedule">
+              <v-icon>
+                mdi-download
+              </v-icon>
+            </v-btn>
+          </template>
+          <span>Guardar horario</span>
+        </v-tooltip>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click="resetCal">
+              <v-icon>
+                mdi-delete
+              </v-icon>
+            </v-btn>
+          </template>
+          <span>Borrar todo</span>
+        </v-tooltip>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click.stop="drawer = !drawer">
+              <v-icon>
+                mdi-book-open
+              </v-icon>
+            </v-btn>
+          </template>
+          <span>Planes de estudio</span>
+        </v-tooltip>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" absolute bottom temporary right>
+        <v-list nav dense>
+          <v-btn block depressed color="error" @click.stop="drawer = !drawer">
+            Cerrar
+          </v-btn>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item v-for="career in syllabuses" :key="career.name">
+              <v-list-item-content>
+                <v-list-item-title>{{ career.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="11">
+              <v-autocomplete
+                :items="items"
+                :error="error"
+                v-model="values"
+                outlined
+                multiple
+                chips
+                deletable-chips
+                small-chips
+                label="Materias"
+              ></v-autocomplete>
+            </v-col>
+
+            <v-col cols="1">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    fab
+                    dark
+                    small
+                    v-bind="attrs"
+                    v-on="on"
+                    color="success"
+                    class="mr-4"
+                    rounded
+                    @click="addCourses"
                   >
-                    <GroupCard
-                      :id="groupNumber"
-                      :group="courses[selected][groupNumber]"
-                      :course="selected"
-                      @clicked="onGroupClicked"
-                    />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-item-group>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-card>
-    </v-dialog>
-    <Calendar :events="summary" @clicked="onEventDeleted" />
+                    <v-icon>
+                      mdi-magnify
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Buscar horarios</span>
+              </v-tooltip>
+            </v-col>
+          </v-row>
+          <Checkout :selectedCourses="summary" />
+        </v-container>
+      </v-card-text>
+
+      <!-- POP UP DIALOG FOR ADDING COURSES -->
+      <v-dialog
+        v-model="dialog"
+        fullscreen
+        hide-overlay
+        transition="dialog-bottom-transition"
+      >
+        <v-card>
+          <v-toolbar dark color="primary">
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+            <v-toolbar-title>Seleccionar horarios</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+              <v-btn dark text @click="submitCourses()">
+                Agregar cursos
+              </v-btn>
+              <v-btn dark text @click="dialog = false">
+                Cancelar
+              </v-btn>
+            </v-toolbar-items>
+
+            <template v-slot:extension>
+              <!-- TABS FOR EACH COURSE -->
+
+              <!-- TAB HEADER -->
+              <v-tabs v-model="tab" align-with-title>
+                <v-tabs-slider color="yellow"></v-tabs-slider>
+                <v-tab v-for="value in values" :key="value">
+                  {{ value.substring(10, value.length) }}
+                </v-tab>
+              </v-tabs>
+            </template>
+          </v-toolbar>
+
+          <!-- TAB BODY -->
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="(selected, s) in values" :key="s">
+              <v-item-group>
+                <v-container fluid>
+                  <v-row no-gutters>
+                    <v-col
+                      v-for="groupNumber in Object.keys(courses[selected])"
+                      :key="groupNumber"
+                      cols="12"
+                      sm="3"
+                    >
+                      <GroupCard
+                        :id="groupNumber"
+                        :group="courses[selected][groupNumber]"
+                        :course="selected"
+                        @clicked="onGroupClicked"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-item-group>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-card>
+      </v-dialog>
+      <Calendar :events="summary" @clicked="onEventDeleted" />
+    </v-card>
   </v-container>
 </template>
 
@@ -195,7 +201,80 @@ export default {
     dialog: false,
     tab: null,
     error: false,
-    isSelecting: false
+    isSelecting: false,
+    drawer: false,
+    syllabuses: [
+      {
+        name: "Actuaría",
+        external:
+          "https://actuaria.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-actuaria-del-ITAM.pdf"
+      },
+      {
+        name: "Administración",
+        external:
+          "https://administracion.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-administracion-del-ITAM.pdf"
+      },
+      {
+        name: "Ciencia de Datos",
+        external:
+          "https://daaem.itam.mx/sites/default/files/programas/planes/plan_ciencia_datos.pdf"
+      },
+      {
+        name: "Ciencia Política",
+        external:
+          "https://politica.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-ciencia-politica-del-ITAM.pdf"
+      },
+      {
+        name: "Contaduría",
+        external:
+          "https://contaduria.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-contaduria-publica-y-estrategia-financiera-del-ITAM.pdf"
+      },
+      {
+        name: "Derecho",
+        external:
+          "http://daedcs.itam.mx/sites/default/files/programas/planes/plan_derecho.pdf"
+      },
+      {
+        name: "Dirección Financiera",
+        external:
+          "https://direccionfinanciera.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-direccion-financiera-del-ITAM.pdf"
+      },
+      {
+        name: "Economía",
+        external:
+          "https://economia.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-economia-del-ITAM.pdf"
+      },
+      {
+        name: "Matemáticas Aplicadas",
+        external:
+          "https://matematicas.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-matematicas-aplicadas-del-ITAM.pdf"
+      },
+      {
+        name: "Relaciones Internacionales",
+        external:
+          "https://relacionesinternacionales.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-licenciatura-en-relaciones-internacionales-del-ITAM.pdf"
+      },
+      {
+        name: "Ing. Computación",
+        external:
+          "https://dai.itam.mx/sites/default/files/programas/planes/plan_ing_computacion.pdf"
+      },
+      {
+        name: "Ing. Industrial",
+        external:
+          "https://dai.itam.mx/sites/default/files/programas/planes/plan_ing_industrial.pdf"
+      },
+      {
+        name: "Ing. Mecatrónica",
+        external:
+          "https://mecatronica.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-ingenieria-en-mecatronica-del-ITAM.pdf"
+      },
+      {
+        name: "Ing. Negocios",
+        external:
+          "https://ingnegocios.itam.mx/sites/default/files/programas/planes/plan-de-estudios-de-la-ingenieria-en-negocios-del-ITAM.pdf"
+      }
+    ]
   }),
   props: {
     courses: Object
